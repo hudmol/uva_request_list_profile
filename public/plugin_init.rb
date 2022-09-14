63f17d2f1e1cf7c2a7f895612eb0296085928717
James Bullen <jambun@gmail.com>
2022-09-14 11:35:07 +1000

    use public_proxy_prefix please

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,4 +1,4 @@
 Plugins::extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
 # set handler url:
-AppConfig[:request_list][:request_handlers][:uva_handler][:url] = "#{AppConfig[:public_proxy_url]}plugin/uva/request"
+AppConfig[:request_list][:request_handlers][:uva_handler][:url] = "#{AppConfig[:public_proxy_prefix]}plugin/uva/request"
